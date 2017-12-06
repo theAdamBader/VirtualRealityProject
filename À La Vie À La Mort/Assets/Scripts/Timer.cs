68b25fad5efbab473368f1f17fa3918385e4cc31
Jason Wan <jwan002@gold.ac.uk>
2017-12-06 16:13:44 +0000

    Ceiling light fixed to be less intense, added an actual lamp, timer script created.

---
+++ À La Vie À La Mort/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    float timeLimit = 30.0f;//Variable for the time limit in seconds. 30 is just an example;change as necessary
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLimit -= Time.deltaTime;//Time deducts per frame.
+        if(timeLimit < 0)//Out of time
+        {
+            //Change GameState here?
+        }
+	}
+}
