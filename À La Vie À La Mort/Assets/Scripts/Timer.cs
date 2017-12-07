6a4c50c7812eb3eb6b43de35d62b45a851ff41a0
Jason Wan <jwan002@gold.ac.uk>
2017-12-07 14:46:42 +0000

    Re-did the timer GUI (previously got lost to the Git kerfuffle). Comes with two fonts to test, can always try more out

--- À La Vie À La Mort/Assets/Scripts/Timer.cs
+++ À La Vie À La Mort/Assets/Scripts/Timer.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+//With reference to https://www.youtube.com/watch?v=D-6BbUozuCY and https://answers.unity.com/questions/1174761/20-minute-countdown-timer.html
+
+
 
 public class Timer : MonoBehaviour {
 
-    float timeLimit = 30.0f;//Variable for the time limit in seconds. 30 is just an example;change as necessary
+    public Text timerText;
+
+    public float timeLimit = 90.0f;//Variable for the time limit in seconds. 90sec/1:30min is just an example;change as necessary
 
 	// Use this for initialization
 	void Start () {
-		
+        timerText = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timeLimit -= Time.deltaTime;//Time deducts per frame.
-        if(timeLimit < 0)//Out of time
+        //Text
+        
+        string min = Mathf.Floor(timeLimit / 60).ToString("00");
+        string sec = (timeLimit % 60).ToString("00");
+        timerText.text = min + ":" + sec;
+
+        if (timeLimit == 0.0f)//Out of time
         {
+
             //Change GameState here?
+        }
+        else
+        {
+            timeLimit -= Time.deltaTime;//Time deducts per frame only if counter is more than 0.
         }
 	}
 }
